f8fc31f84f3dbc65e2b82ed9eb0d4f66e4670a27
Murat <renja.dizz@gmail.com>
2023-11-09 23:34:09 +0600

    add propTypes and defaultProps

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,19 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import {formatDistanceToNow} from 'date-fns'
 import './Task.css';
 
-const Task = ({itemText}) => {
-    const formatedDate = formatDistanceToNow(
-        new Date(itemText.created),
-        {includeSeconds: true}
-    )
-    const dateToShow = `created ${formatedDate} ago`
-    return (
-        <>
-            <span className="description">{itemText.description}</span>
-            <span className="created">{dateToShow}</span>
-        </>
-    );
+
+class Task extends React.Component {
+    render() {
+        const {itemText} = this.props;
+        const formatedDate = formatDistanceToNow(
+            new Date(itemText.created),
+            {includeSeconds: true}
+        )
+        const dateToShow = `created ${formatedDate} ago`
+        return (
+            <>
+                <span className="description">{itemText.description}</span>
+                <span className="created">{dateToShow}</span>
+            </>
+        );
+    }
 }
 
+Task.defaultProps = {
+    description: "Do smthing"
+}
+Task.propTypes = {
+    created: PropTypes.number.isRequired,
+    description: PropTypes.string
+};
 
 export default Task;
