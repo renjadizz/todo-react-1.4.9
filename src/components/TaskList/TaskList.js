7bab300f8278bcd6c858ac81aad90312234283c0
Murat <renja.dizz@gmail.com>
2023-11-09 23:29:05 +0600

    add propTypes and defaultProps

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,31 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import Task from "../Task/Task";
 import './TaskList.css';
 
 
-const TaskList = ({taskItems, onChangeTaskState, onDeleteTaskItem}) => {
-    const taskItem = taskItems.map((item) => {
-        const {id, state, ...itemText} = item;
+export default class TaskList extends React.Component {
+
+    render() {
+        const {taskItems, onChangeTaskState, onDeleteTaskItem} = this.props;
+        const taskItem = taskItems.map((item) => {
+            const {id, state, ...itemText} = item;
+            return (
+                <li key={id} className={state}>
+                    <div className="view">
+                        <input className="toggle" type="checkbox"
+                               checked={state === 'completed' ? true : false}
+                               onChange={() => onChangeTaskState(id)}/>
+                        <label><Task itemText={itemText}/></label>
+                        <button className="icon icon-edit"></button>
+                        <button className="icon icon-destroy" onClick={() => onDeleteTaskItem(id)}></button>
+                    </div>
+                    {state === 'editing' ? <input type="text" className="edit" defaultValue="Editing task"/> : null}
+                </li>
+            );
+        });
         return (
-            <li key={id} className={state}>
-                <div className="view">
-                    <input className="toggle" type="checkbox"
-                           checked={state === 'completed' ? true : false}
-                           onChange={() => onChangeTaskState(id)}/>
-                    <label><Task itemText={itemText}/></label>
-                    <button className="icon icon-edit"></button>
-                    <button className="icon icon-destroy" onClick={() => onDeleteTaskItem(id)}></button>
-                </div>
-                {state === 'editing' ? <input type="text" className="edit" defaultValue="Editing task"/> : null}
-            </li>
+            <ul className="todo-list">
+                {taskItem}
+            </ul>
         );
-    });
-
-    return (
-        <ul className="todo-list">
-            {taskItem}
-        </ul>
-    );
+    }
 }
-
-
-export default TaskList;
+TaskList.defaultProps = {
+    onChangeTaskState: () => {
+    },
+    onDeleteTaskItem: () => {
+    },
+}
+TaskList.propTypes = {
+    taskItems: PropTypes.object.isRequired,
+    onChangeTaskState: PropTypes.func,
+    onDeleteTaskItem: PropTypes.func
+};
