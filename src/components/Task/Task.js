a37bfb8d735f9e2e4bcb9a5912d9fe3632f5f817
Murat <renja.dizz@gmail.com>
2023-11-09 22:06:04 +0600

    format date to show

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,10 +1,16 @@
+import {formatDistanceToNow} from 'date-fns'
 import './Task.css';
 
 const Task = ({itemText}) => {
+    const formatedDate = formatDistanceToNow(
+        new Date(itemText.created),
+        {includeSeconds: true}
+    )
+    const dateToShow = `created ${formatedDate} ago`
     return (
         <>
             <span className="description">{itemText.description}</span>
-            <span className="created">{itemText.created}</span>
+            <span className="created">{dateToShow}</span>
         </>
     );
 }
